791c7f35c8d63f716829e6bc505dd1109e007f45
Federico <fkatzaroff@gmail.com>
2019-07-06 21:25:27 -0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
 // vue.config.js
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 module.exports = {
   // options...
   chainWebpack: config => config.plugins.delete('named-chunks'),
+  configureWebpack: {
+    plugins: [
+      new SWPrecacheWebpackPlugin({
+        cacheId: 'caym',
+        filename: 'service-worker.js',
+        staticFileGlobs: [
+          'dist/**/*.{js,css}',
+          'manifest.json',
+          '/'
+        ],
+        minify: true,
+        stripPrefix: 'dist/',
+        dontCacheBustUrlsMatching: /\.\w{6}\./
+      })
+    ]},
   devServer: {
     proxy: {
       '^/api': {
